cef954dc27547606e8c02549bab750e029572645
FilmBee <92419399+FilmBee@users.noreply.github.com>
2023-06-29 12:53:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,28 +4,14 @@ document.getElementById('save-button').addEventListener('click', function() {
   // Generate a random filename for the text file
   var filename = generateFilename();
 
-  // Create a Blob object with the text content
-  var blob = new Blob([text], { type: 'text/plain' });
-
-  // Create a temporary <a> element to generate the download link
-  var link = document.createElement('a');
-  link.href = URL.createObjectURL(blob);
-  link.download = filename;
-
-  // Append the link to the page
-  document.body.appendChild(link);
-
-  // Trigger a click event to initiate the download
-  link.click();
-
-  // Remove the temporary link from the page
-  document.body.removeChild(link);
+  // Save the text content in local storage
+  localStorage.setItem(filename, text);
 
   // Display the link container
   document.getElementById('link-container').style.display = 'block';
 
   // Set the input value to the shareable link
-  document.getElementById('link-input').value = window.location.href + filename;
+  document.getElementById('link-input').value = window.location.href + '#' + filename;
 });
 
 function generateFilename() {
