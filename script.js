92493fc2e5749976205b76db42df7044ecab0c54
FilmBee <92419399+FilmBee@users.noreply.github.com>
2023-06-29 13:09:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 document.getElementById('save-button').addEventListener('click', function() {
   var text = document.getElementById('text-input').value;
 
-  // Generate a random filename for the text file
-  var filename = generateFilename();
-
-  // Construct the content for the Telegra.ph article
-  var content = "<pre>" + text + "</pre>";
-
-  // Open Telegra.ph in a new tab with the content
-  window.open("https://telegra.ph/" + filename, "_blank");
-
-  // Display the link container
-  document.getElementById('link-container').style.display = 'block';
-
-  // Set the input value to the shareable link
-  document.getElementById('link-input').value = "https://telegra.ph/" + filename;
-});
-
-function generateFilename() {
-  var chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  var filename = '';
-  for (var i = 0; i < 8; i++) {
-    var randomIndex = Math.floor(Math.random() * chars.length);
-    filename += chars[randomIndex];
+  // Prompt the user for the desired file name
+  var filename = prompt("Enter a file name", "paste.txt");
+  if (filename === null || filename.trim() === "") {
+    return; // Cancelled or empty file name, do nothing
   }
-  return filename;
-}
+
+  // Create a Blob object with the text content
+  var blob = new Blob([text], { type: 'text/plain' });
+
+  // Create a temporary <a> element to generate the download link
+  var link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+
+  // Append the link to the page
+  document.body.appendChild(link);
+
+  // Trigger a click event to initiate the download
+  link.click();
+
+  // Remove the temporary link from the page
+  document.body.removeChild(link);
+});
