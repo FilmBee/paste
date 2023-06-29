fb5c37306007f1961d938c9d10ab8f376e607df9
FilmBee <92419399+FilmBee@users.noreply.github.com>
2023-06-29 12:56:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ document.getElementById('save-button').addEventListener('click', function() {
   // Generate a random filename for the text file
   var filename = generateFilename();
 
-  // Save the text content in local storage
-  localStorage.setItem(filename, text);
+  // Construct the content for the Telegra.ph article
+  var content = "<pre>" + text + "</pre>";
+
+  // Open Telegra.ph in a new tab with the content
+  window.open("https://telegra.ph/" + filename, "_blank");
 
   // Display the link container
   document.getElementById('link-container').style.display = 'block';
 
   // Set the input value to the shareable link
-  document.getElementById('link-input').value = window.location.href + '#' + filename;
+  document.getElementById('link-input').value = "https://telegra.ph/" + filename;
 });
 
 function generateFilename() {
@@ -21,5 +24,5 @@ function generateFilename() {
     var randomIndex = Math.floor(Math.random() * chars.length);
     filename += chars[randomIndex];
   }
-  return filename + '.txt';
+  return filename;
 }
